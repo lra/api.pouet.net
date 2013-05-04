625f9b1553c4cef3e04259a4ed39c02bc2a7f97d
Laurent Raufaste <analogue@glop.org>
2013-05-03 21:58:26 -0400

    Let's use Flask-RESTful

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-import os
 from flask import Flask
+from flask.ext import restful
 
 app = Flask(__name__)
+api = restful.Api(app)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
